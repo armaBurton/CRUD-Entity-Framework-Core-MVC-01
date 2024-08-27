1662f0a89f8254028ce07ed18c0e086bcb4c88b9
Arma Burton <arma@armaburton.com>
2024-08-26 23:04:57 -0700

    DbSettings.cs

---
+++ Helpers/DbSettings.cs
@@ -0,0 +1,8 @@
+namespace CRUD_Entity_Framework_Core_MVC_01.Helpers;
+
+public class DbSettings{
+    public string? Server { get; set; }
+    public string? Database { get; set; }
+    public string? UserId { get; set; }
+    public string? Password { get; set; }
+}
