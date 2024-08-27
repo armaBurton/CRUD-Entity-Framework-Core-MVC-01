42bd31ddd1ff76258a6897da7d4c0b60fd880f0b
Arma Burton <arma@armaburton.com>
2024-08-26 22:08:40 -0700

    services

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
+using System.Text.Json.Serialization;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+{
+    var services = builder.Services;
+    var env = builder.Environment;
 
+    services.AddCors();
+    services.AddControllers().AddJsonOptions(x => {
+        // serialize enums as strings in api responses
+        x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+        //ignore omitted parameters on models to enable optional params (e.g. User update)
+        x.JsonSerializerOptions.DefaultIgnoreCondition.Add(new JsonStringEnumConverter());
+    });
+    services.AddAutoMapper(AppDomain, CurrentDomain, GetAssemblies());
+
+    // configure strongly typed settings object
+    services.Configure<DbSettings>(builder.Configuration.GetSection("DbSettings"));
+
+    //configure container for applications services
+    services.AddSingleton<DataContext>();
+    services.AddScoped()<IKaraokeReopsitory, KaraokeRepository>();
+}
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
